f8ecc8216f8a6e039871534927a1b9f1c7e22cae
Osazie <uyiekpenelizabeth@gmail.com>
2023-10-25 16:17:18 +0100

    new updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="./pix2.jpg" type="image/jpg" sizes="32x32" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
